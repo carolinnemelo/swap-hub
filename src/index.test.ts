4c1b872460afa930330f0910091e8943280f9f25
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-01 21:13:32 +0100

    add test for suppertest

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,1 +1,11 @@
-import test from "node:test"
+import express from "express";
+import { deepEqual } from "node:assert";
+import test from "node:test";
+import request from "supertest";
+
+test("supertest works!", async () => {
+  const app = express();
+  const result = await request(app).get("/status");
+
+  deepEqual(result.status, 200);
+});
