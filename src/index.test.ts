4554e239f76a082396271a61028ae64c2650293f
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-01 22:18:17 +0100

    supertest works using createApp method and not creating an new express app

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,12 +2,10 @@ import express from "express";
 import { deepEqual } from "node:assert";
 import test from "node:test";
 import request from "supertest";
+import { createApp } from "./app";
 
 test("supertest works!", async () => {
-  const app = express();
-  app.get("/status", (req, res) => {
-    res.status(200).end();
-  });
+  const app = createApp()
   const result = await request(app).get("/status");
 
   deepEqual(result.status, 200);
