e4594adaa9db5aecc4177eb10b7633aecfddee02
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-01 22:12:28 +0100

    supertest works

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,6 +5,9 @@ import request from "supertest";
 
 test("supertest works!", async () => {
   const app = express();
+  app.get("/status", (req, res) => {
+    res.status(200).end();
+  });
   const result = await request(app).get("/status");
 
   deepEqual(result.status, 200);
