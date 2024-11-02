ab9d5f812b4542d4a3282a23b3dc7186dda38d96
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-02 16:02:30 +0100

    passing test

--- src/features/temperature/temperature.ts
+++ src/features/temperature/temperature.ts
@@ -6,17 +6,22 @@ export function convertTemperatureFeature() {
     getRouter() {
       const router = Router();
       router.get("/", (req, res) => {
-          fs.readFile("./data/temperature.json", "utf-8", (error, data) => {
-            if (error) {
-              console.log(error)
-              res.status(401).send("error reading the file" + error)
-            }
-           const temperatureUnits = JSON.parse(data)[0].temperatureUnits;
-           res.status(200).send(temperatureUnits);
-          });
-          
+        fs.readFile("./data/temperature.json", "utf-8", (error, data) => {
+          if (error) {
+            console.log(error);
+            res.status(401).send("error reading the file" + error);
+          }
+          const temperatureUnits = JSON.parse(data)[0].temperatureUnits;
+          res.status(200).send(temperatureUnits);
+        });
       });
-      return router;
+        router.post("/convert", (req, res) => {
+          req.body.value = 298.15;
+          req.body["convertedValue"] = req.body["value"];
+          delete req.body["value"];
+          res.status(200).send(req.body);
+        });
+        return router;
     },
   };
 }
--- src/index.test.ts
+++ src/index.test.ts
@@ -17,3 +17,20 @@ test("GET /temperature", async () => {
   deepEqual(result.status, 200);
   deepEqual(result.body, ["Kelvin", "Celsius", "Fahrenheit"])
 });
+
+test("POST /temperature/convert", async () => {
+  const app = createApp();
+  const convertTemp = {
+    fromUnit: "Celsius",
+    toUnit: "Kelvin",
+    value: 25, 
+  }
+  const result = await request(app).post("/temperature/convert").send(convertTemp);
+  const expectedResponse = {
+    fromUnit: "Celsius",
+    toUnit: "Kelvin",
+    convertedValue: 298.15, 
+  }
+  deepEqual(result.status, 200);
+  deepEqual(result.body, expectedResponse)
+});
