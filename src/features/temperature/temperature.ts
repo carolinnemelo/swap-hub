dd17afd12b782a26f6ec8d3a6da0ed7653e81405
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-02 12:57:06 +0100

    remove async from /temperature/

--- src/features/temperature/temperature.ts
+++ src/features/temperature/temperature.ts
@@ -5,11 +5,11 @@ export function convertTemperatureFeature() {
   return {
     getRouter() {
       const router = Router();
-      router.get("/", async (req, res) => {
+      router.get("/", (req, res) => {
           fs.readFile("./data/temperature.json", "utf-8", (error, data) => {
             if (error) {
               console.log(error)
-              res.status(401).send("error reading the file")
+              res.status(401).send("error reading the file" + error)
             }
            const temperatureUnits = JSON.parse(data)[0].temperatureUnits;
            res.status(200).send(temperatureUnits);
