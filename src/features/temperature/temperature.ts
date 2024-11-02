c1d28600dc0102357c12bb16948492c7fa296e80
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-02 12:50:10 +0100

    getting json from /temperature endpoint

--- src/features/temperature/temperature.ts
+++ src/features/temperature/temperature.ts
@@ -1,15 +1,23 @@
 import { Router } from "express";
+import fs from "fs";
 
 export function convertTemperatureFeature() {
   return {
     getRouter() {
       const router = Router();
-      router.get("/", (req, res) => {
-        res.status(200);
-        res.send("from router")
+      router.get("/", async (req, res) => {
+          fs.readFile("./data/temperature.json", "utf-8", (error, data) => {
+            if (error) {
+              console.log(error)
+              res.status(401).send("error reading the file")
+            }
+            const parsedData = JSON.parse(data);
+            res.status(200).send(parsedData);
+          });
+          
       });
       return router;
     },
-  }; 
+  };
 }
 
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,20 +1,19 @@
-import express from "express";
 import { deepEqual } from "node:assert";
 import test from "node:test";
 import request from "supertest";
 import { createApp } from "./app";
 
 test("supertest works!", async () => {
-  const app = createApp()
+  const app = createApp();
   const result = await request(app).get("/");
 
   deepEqual(result.status, 200);
 });
 
-
 test("GET /temperature", async () => {
-  const app = createApp()
+  const app = createApp();
   const result = await request(app).get("/temperature");
 
   deepEqual(result.status, 200);
+  deepEqual(result.body, ["Kelvin", "Celsius", "Fahrenheit"])
 });
