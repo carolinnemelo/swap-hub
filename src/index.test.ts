f8791ffc384caadbb7f165f8ddb8787ecdf67c50
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 12:45:55 +0100

    change test to use logic function and prepare to use zod

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,7 @@ import { deepEqual } from "node:assert";
 import test from "node:test";
 import request from "supertest";
 import { createApp } from "./app";
+import { convertTemperature } from "./features/temperature/logic";
 
 test("supertest works!", async () => {
   const app = createApp();
@@ -15,18 +16,20 @@ test("GET /temperature", async () => {
   const result = await request(app).get("/temperature");
 
   deepEqual(result.status, 200);
-  deepEqual(result.body, ["Kelvin", "Celsius", "Fahrenheit"])
+  deepEqual(result.body, ["Kelvin", "Celsius", "Fahrenheit"]);
 });
 
 test("POST /temperature/convert", async () => {
   const app = createApp();
-  const convertTemperature = {
+  const testConvertTemperature = {
     fromUnit: "C",
     toUnit: "F",
-    value: 25, 
-  }
-  const result = await request(app).post("/temperature/convert").send(convertTemperature);
-
+    value: 25,
+  };
+  const result = await request(app)
+    .post("/temperature/convert")
+    .send(testConvertTemperature);
+  const expectedValue = convertTemperature("C", "F", 25);
   deepEqual(result.status, 200);
-  deepEqual(result.body.convertedValue, 77)
+  deepEqual(result.body.convertedValue, expectedValue);
 });
