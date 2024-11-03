61883d0873cc0730fc349521978dbcd271a210f0
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 09:19:32 +0100

    create a route for units

--- src/features/temperature/temperature.ts
+++ src/features/temperature/temperature.ts
@@ -18,6 +18,17 @@ export function convertTemperatureFeature() {
         });
       });
 
+      router.get("/units", (req, res) => {
+        fs.readFile("./data/temperature.json", "utf-8", (error, data) => {
+          if (error) {
+            console.log(error);
+            res.status(401).send("error reading the file" + error);
+          }
+          const temperatureUnits = JSON.parse(data)[0].temperatureUnits;
+          res.status(200).send(temperatureUnits);
+        });
+      });
+
       router.post("/convert", (req, res) => {
         const { fromUnit, toUnit, value } = req.body
         const convertedValue = convertTemperature(fromUnit, toUnit, value);
