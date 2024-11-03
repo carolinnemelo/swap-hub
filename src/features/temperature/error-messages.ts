fb07c9630680b7053661e9439ff8e7f8ac732aeb
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 12:03:42 +0100

    create a file with the errors messages

---
+++ src/features/temperature/error-messages.ts
@@ -0,0 +1,5 @@
+export const temperatureErrors = {
+    kelvin: "Kelvin can't be a negative value",
+    celsius: "Celsius can't have a value bellow -273.15",
+    fahrenheit: "Fahrenheit can't have a value bellow -459.67"
+}
