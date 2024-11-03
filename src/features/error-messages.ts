7347926e45f594ba8deac63f3312243323c3050a
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 17:27:18 +0100

    move to features folder

---
+++ src/features/error-messages.ts
@@ -0,0 +1,8 @@
+export const temperatureErrors = {
+  kelvin: "Kelvin can't be a negative value",
+  celsius: "Celsius can't have a value bellow -273.15",
+  fahrenheit: "Fahrenheit can't have a value bellow -459.67",
+  invalidUnit:
+    "Invalid unit. Accepted units are Celsius, Fahrenheit, and Kelvin.",
+  missingParameters: "All three parameters are required.",
+};
