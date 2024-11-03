1e9a07b3884e56a8ada15b5af87a68711c476137
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 12:16:39 +0100

    add invalid unit message

--- src/features/temperature/error-messages.ts
+++ src/features/temperature/error-messages.ts
@@ -1,5 +1,6 @@
 export const temperatureErrors = {
     kelvin: "Kelvin can't be a negative value",
     celsius: "Celsius can't have a value bellow -273.15",
-    fahrenheit: "Fahrenheit can't have a value bellow -459.67"
+    fahrenheit: "Fahrenheit can't have a value bellow -459.67",
+    invalidUnit: "Invalid unit. Accepted units are Celsius, Fahrenheit, and Kelvin."
 }
