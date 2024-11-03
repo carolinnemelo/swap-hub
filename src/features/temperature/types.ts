7adb8e1b590c1fefa1eab52083702cf02b0ba0f2
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 14:00:45 +0100

    add min value to zod validations

--- src/features/temperature/types.ts
+++ src/features/temperature/types.ts
@@ -4,7 +4,7 @@ import { isTemperatureRangeValid, normalizeUnit } from "./logic";
 type ConvertObject = z.infer<typeof temperatureSchema>
 
 export const temperatureSchema = z.object({
-  fromUnit: z.string().max(10),
-  toUnit: z.string().max(10),
+  fromUnit: z.string().max(10).min(1),
+  toUnit: z.string().max(10).min(1),
   value: z.string(),
 });
