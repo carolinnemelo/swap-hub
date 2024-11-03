b5f0112c3fcc6cca4a42aac76a86fad765106a79
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 11:04:27 +0100

    normalize unit on zod

--- src/features/temperature/types.ts
+++ src/features/temperature/types.ts
@@ -1,9 +1,16 @@
 import { z } from "zod";
+import { normalizeUnit } from "./logic";
 
 type ConvertObject = z.infer<typeof temperatureSchema>
 
 export const temperatureSchema = z.object({
-  fromUnit: z.string(),
-  toUnit: z.string(),
-  value: z.number(),
+  fromUnit: z
+    .string()
+    .max(10)
+    .transform((unit) => normalizeUnit(unit)),
+  toUnit: z
+    .string()
+    .max(10)
+    .transform((unit) => normalizeUnit(unit)),
+  value: z.number().transform(() => {}),
 });
