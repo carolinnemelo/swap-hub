97975acfba7823e963934b219fea70a47cb3c952
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 13:43:56 +0100

    remove transforms on temperatureSchema cause transform should not throw an error

--- src/features/temperature/types.ts
+++ src/features/temperature/types.ts
@@ -4,13 +4,7 @@ import { isTemperatureRangeValid, normalizeUnit } from "./logic";
 type ConvertObject = z.infer<typeof temperatureSchema>
 
 export const temperatureSchema = z.object({
-  fromUnit: z
-    .string()
-    .max(10)
-    .transform((fromUnit) => normalizeUnit(fromUnit)),
-  toUnit: z
-    .string()
-    .max(10)
-    .transform((toUnit) => normalizeUnit(toUnit)),
-  value: z.number(),
+  fromUnit: z.string().max(10),
+  toUnit: z.string().max(10),
+  value: z.string(),
 });
