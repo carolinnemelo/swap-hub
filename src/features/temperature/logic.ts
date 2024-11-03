f275e3ae85181dfbff5402bf05007602d72cd407
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 09:57:32 +0100

    add logic for zero case

--- src/features/temperature/logic.ts
+++ src/features/temperature/logic.ts
@@ -1,6 +1,9 @@
 import convert, { Unit } from "convert-units";
 
 export function convertTemperature(fromUnit, toUnit, value: number) {
+    if(!fromUnit || !toUnit || !value) {
+        return "fail"
+    }
 
   const convertedValue = convert(value).from(fromUnit).to(toUnit);
 
