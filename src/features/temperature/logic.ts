b61a3f66fd28e162c0b3d1ac4e4cc9ce73cd541e
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-03 10:26:30 +0100

    correct ifs that were not being compared with the variable

--- src/features/temperature/logic.ts
+++ src/features/temperature/logic.ts
@@ -1,13 +1,27 @@
 import convert, { Unit } from "convert-units";
 
 export function convertTemperature(fromUnit, toUnit, value: number) {
-    if(!fromUnit || !toUnit || !value) {
-        return "fail"
-    }
-
+  if (!fromUnit || !toUnit || !value) {
+    return "fail";
+  }
+  fromUnit = normalizeUnit(fromUnit);
+  toUnit = normalizeUnit(toUnit);
   const convertedValue = convert(value).from(fromUnit).to(toUnit);
 
   return convertedValue;
 }
 
+export function normalizeUnit(unit: string) {
+  unit = unit.toLowerCase();
+  if (unit === "c" || unit ==="celsius") {
+    return "C";
+  }
+  if (unit === "f" || unit ==="fahrenheit") {
+    return "F";
+  }
+  if (unit === "k" || unit ==="kelvin") {
+    return "K";
+  }
 
+  return "Not a acceptable unit";
+}
