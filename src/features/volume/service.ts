8a09994b87dfa236eeba3d62ca82f335db939d61
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-07 20:22:53 +0100

    change router

--- src/features/volume/service.ts
+++ src/features/volume/service.ts
@@ -2,10 +2,12 @@ import { v4 } from "uuid";
 import fs from "fs";
 import { volumeErrors } from "../error-messages";
 import { normalizeUnit, parseValue } from "./logic";
+import { Db } from "./db";
 
 export function getVolumeUnits() {
   try {
     const data = JSON.parse(fs.readFileSync("./data/volume.json", "utf-8"));
+    //getJsonContent(fileName) 
     return data[0].volumeUnits;
   } catch {
     throw new Error(volumeErrors.fileReadError);
@@ -26,7 +28,7 @@ export function parseVolumeInputs(req, volumeUnits) {
 
 export function saveConversion({ fromUnit, toUnit, value, convertedValue }) {
   const id = generateId();
-  const { filePath, time } = createConversionFilePerDay();
+  const { filePath, time } = createConversionFilePerDay(); //colocar a funcao no novo file
   const conversion = {
     id,
     time,
@@ -35,17 +37,17 @@ export function saveConversion({ fromUnit, toUnit, value, convertedValue }) {
     toUnit,
     convertedValue,
   };
-  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const fileContent = fs.readFileSync(filePath, "utf-8"); // usar a funcao readJsonContent(fileName)
   const currentConversions = JSON.parse(fileContent);
   currentConversions.push(conversion);
-  fs.writeFileSync(filePath, JSON.stringify(currentConversions, null, 2));
+  fs.writeFileSync(filePath, JSON.stringify(currentConversions, null, 2));// writeJsonContent(file)
 }
 
 export function generateId() {
   return v4();
 }
 
-export function createConversionFilePerDay() {
+export function createConversionFilePerDay() { //move pro novo arquivo
   if (!fs.existsSync("data/volume-conversions-day")) {
     fs.mkdirSync("data/volume-conversions-day", { recursive: true });
   }
@@ -58,4 +60,25 @@ export function createConversionFilePerDay() {
     fs.writeFileSync(filePath, "[]", "utf-8");
   }
   return { filePath, time };
+}
+
+
+export function createService(db: Db) {
+  async function viewAll() {
+    return db.viewAll()
+  }
+
+  async function parseVolumeInputs(req, volumeUnits) {
+    let { fromUnit, toUnit, value } = req;
+    try {
+      fromUnit = normalizeUnit(fromUnit, volumeUnits);
+      toUnit = normalizeUnit(toUnit, volumeUnits);
+      value = parseValue(value);
+      return { fromUnit, toUnit, value };
+    } catch (error) {
+      return error.message;
+    }
+  }
+  
+  
 }
