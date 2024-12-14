205628235a03bb37f5a8cb6196ebec275dee5360
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-14 12:41:36 +0100

    remove argument from get history and add get history by date to repository layer

--- src/features/volume/repository.ts
+++ src/features/volume/repository.ts
@@ -3,17 +3,27 @@ import fs from "fs";
 
 export function createRepository() {
   return {
-    async getHistory(filePath: string) {
+    async getHistory() {
       return new Promise((resolve, reject) => {
-        fs.readFile(filePath, "utf-8", (error, data) => {
-          if (error) {
-            reject(error);
-            return;
-          }
-          resolve(data);
-        });
+        const files = fs.readdirSync("data/volume-conversions-day");
+        if (!files) {
+          reject();
+          return;
+        }
+        resolve(files);
       });
-    }
+    },
 
-  }
+    async getHistoryByDate(date: string) {
+      return new Promise((resolve, reject) => {
+        const files = fs.readdirSync("data/volume-conversions-day");
+        const file = files.find((file) => file === `${date}.json`);
+        if (!file) {
+          reject();
+          return;
+        }
+        resolve(file);
+      });
+    },
+  };
 }
