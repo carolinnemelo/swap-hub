d27d5598e4966c392edb381e2c7a2cb73836178e
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-14 12:36:42 +0100

    add repository type infering it

--- src/features/volume/types.ts
+++ src/features/volume/types.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 import { volumeSchema } from "./z-schema";
 import { createService } from "./service";
+import { createRepository } from "./repository";
 
 export type VolumeSchemaObject = z.infer<typeof volumeSchema>;
 
+export type Repository = ReturnType< typeof createRepository> ;
 export type Service = ReturnType<typeof createService>;
